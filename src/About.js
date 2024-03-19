6c9bff17efdce21291c2457876de0c029aad4fc4
satheesh2705 <mrsatheesh2705@gmail.com>
2024-03-19 16:46:42 +0530

    detail changes

--- src/About.js
+++ src/About.js
@@ -8,12 +8,13 @@ const About = () => {
             
             <a href='mrsatheesh2705@gmail.com'><p>Gmail:SATHEESH S</p></a>
             <Link to="http://www.linkedin.com/in/satheeshsaravanan/"><p>Linkeedin:Satheesh</p> </Link>
+            <Link to="https://github.com/satheesh2705/"><p>Github:satheesh2705</p></Link>
              <p>
                 Mobile No: 6383591766
              </p>
             
             <p>Instagram:mr_black</p>
-            <p style={{ marginTop: "1rem" }}>Hi, I am Satheesh, From Kallai, Together lets make and get free education for everyone in the world </p>
+            <p style={{ marginTop: "1rem" }} >Hi, I am Satheesh, From Kallai, Together lets make and get free education for everyone in the world </p>
         </main>
     )
 }
